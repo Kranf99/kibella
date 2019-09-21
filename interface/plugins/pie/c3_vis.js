62982ac3e91a8e75725fa84944279be46b193895
leo <mr.bourgueleo@gmail.com>
2019-09-21 20:37:04 +0200

    change pie legend to showed by default

--- interface/plugins/pie/c3_vis.js
+++ interface/plugins/pie/c3_vis.js
@@ -22,11 +22,11 @@ define(function (require) {
         params: {
           defaults: {
             addTooltip: true,
-            addLegend: false,
+            addLegend: true,
             isDonut: false,
             displayOther: true
           },
-              editor: c3VisParamsTemplate
+          editor: c3VisParamsTemplate
         },
         hierarchicalData: true,
         schemas: new Schemas([
@@ -66,79 +66,3 @@ define(function (require) {
 
   return pieVisProvider;
 });
-
-
-
-
-// define(function (require) {
-//   require('plugins/k5p-c3/c3_vis.less');
-//   require('plugins/k5p-c3/c3_vis_controller');
-//   var TemplateVisTypeTemplateVisTypeProvider = require('ui/template_vis_type/TemplateVisType');
-//   var VisSchemasProvider = require('ui/Vis/Schemas');
-//   var c3VisTemplate = require('plugins/k5p-c3/c3_vis.html');
-//   var c3VisParamsTemplate = require('plugins/k5p-c3/c3_vis_params.html');
-
-//   // register the provider with the visTypes registry
-//   require('ui/registry/vis_types').register(c3VisProvider);
-
-//   return function c3VisProvider(Private) {
-//     var VislibVisType = Private(require('plugins/vis_types/vislib/_vislib_vis_type'));
-//     var Schemas = Private(require('plugins/vis_types/_schemas'));
-//     var TemplateVisType = Private(TemplateVisTypeTemplateVisTypeProvider);
-//        var Schemas = Private(VisSchemasProvider);
-    
-//     return new TemplateVisType({
-//    // return new VislibVisType({
-//       name: 'pie_chart',
-//       title: 'Pie',
-//       icon: 'fa-pie-chart',
-//       description: 'Pie charts are ideal for displaying the parts of some whole. For example, sales percentages by department.' +
-//        'Pro Tip: Pie charts are best used sparingly, and with no more than 7 slices per pie.',
-//       params: {
-//         defaults: {
-//           shareYAxis: true,
-//           addTooltip: true,
-//           addLegend: true,
-//           isDonut: false,
-//           displayOther: true
-//         },
-//         editor: require('text!plugins/vis_types/vislib/editors/pie.html')
-//       },
-//       responseConverter: false,
-//       hierarchicalData: true,
-//       schemas: new Schemas([
-//         {
-//           group: 'metrics',
-//           name: 'metric',
-//           title: 'Slice Size',
-//           min: 1,
-//           max: 1,
-//           aggFilter: ['sum', 'count', 'cardinality'],
-//           defaults: [
-//             { schema: 'metric', type: 'count' }
-//           ]
-//         },
-//         {
-//           group: 'buckets',
-//           name: 'segment',
-//           icon: 'fa fa-scissors',
-//           title: 'Split Slices',
-//           min: 0,
-//           max: Infinity,
-//           aggFilter: '!geohash_grid'
-//         },
-//         {
-//           group: 'buckets',
-//           name: 'split',
-//           icon: 'fa fa-th',
-//           title: 'Split Chart',
-//           mustBeFirst: true,
-//           min: 0,
-//           max: 1,
-//           aggFilter: '!geohash_grid'
-//         }
-//       ])
-//     });
-
-//   };
-// });
